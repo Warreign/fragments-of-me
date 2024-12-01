9ae351e7c2041aecddddf59a68e39c9ed4dd50d8
Warreign <warreignn@gmail.com>
2024-12-01 14:56:00 +0100

    add player animation

--- player.gd
+++ player.gd
@@ -1,7 +1,19 @@
 class_name Player
 extends CharacterBody2D
 
+enum DIRECTION
+{
+	LEFT,
+	RIGHT,
+	UP,
+	DOWN
+}
+
+var direction : DIRECTION = DIRECTION.DOWN
+
 @export_file("*.tscn") var next_scene
+
+@onready var sprite : AnimatedSprite2D = $AnimatedSprite2D
 
 var current_interactable : Interactable = null
 
@@ -11,6 +23,7 @@ var reach_area : Area2D
 
 func _ready():
 	reach_area = $ReachArea
+	sprite.play()
 
 func _process(delta):
 	if (Controller.is_handling_movement()):
@@ -29,9 +42,38 @@ func _get_inputs():
 	if (!Controller.is_handling_movement()):
 		return
 	velocity = Input.get_vector("move_left", "move_right", "move_up", "move_down") * SPEED
+	
+	if (Input.is_action_pressed("move_down")):
+		direction = DIRECTION.DOWN
+	if (Input.is_action_pressed("move_up")):
+		direction = DIRECTION.UP
+	if (Input.is_action_pressed("move_left")):
+		direction = DIRECTION.LEFT
+	if (Input.is_action_pressed("move_right")):
+		direction = DIRECTION.RIGHT
+		
+		
+		
+func get_dir_str():
+	if (direction == DIRECTION.DOWN):
+		return "down"
+	if (direction == DIRECTION.UP):
+		return "up"
+	if (direction == DIRECTION.LEFT):
+		return "left"
+	if (direction == DIRECTION.RIGHT):
+		return "right"
+		
+func update_animation():
+	if (velocity == Vector2.ZERO):
+		sprite.play("standing_" + get_dir_str())
+	else:
+		sprite.play("walking_" + get_dir_str())
+	
 
 func _physics_process(delta: float) -> void:
 	_get_inputs()
+	update_animation()
 	move_and_slide()
 	
 func _on_interactable_enter(interactable_area : Area2D):

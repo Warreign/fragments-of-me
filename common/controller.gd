1803a2e6506475781eeb66f76853690a4240e63e
Warreign <warreignn@gmail.com>
2024-12-01 14:33:47 +0100

    add pause menu

--- common/controller.gd
+++ common/controller.gd
@@ -1,35 +1,42 @@
 extends Node2D
 
+@export_file("*.tscn") var menu_scene
+var is_in_menu : bool = true
+
 @export_file("*.tscn") var start_scene
 var current_scene : Node = null
 
 var _is_handling_movement : bool = true
 
-@export var player : Player = preload("res://Player.tscn").instantiate()
+@export var player : Player
 
 func _ready():
 	current_scene = get_tree().root.get_node("MainMenu")
 	assert(current_scene)
 	pass
 	
-func goto_scene(scene_name : String, init_position : Vector2 = Vector2.INF) -> void:
-	call_deferred("_deferred_goto_scene", scene_name, init_position);
+func goto_scene(scene_name : String, move_player :bool = true) -> void:
+	call_deferred("_deferred_goto_scene", scene_name, move_player);
 	
-func _deferred_goto_scene(scene_name : String, init_position : Vector2) -> void:
+func _deferred_goto_scene(scene_name : String, move_player : bool) -> void:
+	if (move_player):
+		is_in_menu = false
+	else:
+		is_in_menu = true
+	
 	if (current_scene):
-		current_scene.remove_child(player)
+		if (move_player):
+			current_scene.remove_child(player)
 		get_tree().root.remove_child(current_scene);
 		current_scene.free();
 	var s = ResourceLoader.load(scene_name)
 	current_scene = s.instantiate()
 	get_tree().root.add_child(current_scene);
-	current_scene.add_child(player)
-	player.position = (current_scene as Room).last_position
 	
+	if (move_player):
+		current_scene.add_child(player)
+		player.position = (current_scene as Room).last_position
 	
-	if (init_position != Vector2.INF):
-		(current_scene.get_node("Player") as Node2D).position = init_position;
-		
 	
 func set_handling_movement(value : bool):
 	_is_handling_movement = value
@@ -48,4 +55,7 @@ func start_game():
 	
 func quit_game():
 	get_tree().quit()
+	
+func goto_main_menu():
+	goto_scene(menu_scene, false)
 	
